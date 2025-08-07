1486112e982938d8d0aee68a12bc517c677eca44
David Sklarčík <sklarcik.david@gmail.com>
2025-08-07 22:24:07 +0200

    Files push

---
+++ recommendator.py
@@ -0,0 +1,56 @@
+import discord
+from discord.ext import commands
+import requests
+
+bot = commands.Bot(command_prefix='!', intents=discord.Intents.default())
+
+def ziskej_info_o_filmu(nazev_filmu, api_key):
+    url: f'http://www.omdbapi.com/?t={nazev_filmu}&apikey={api_klic}&plot=short'
+    response = requests.get(url)
+    data = response.json()
+    if data['Response'] == 'True':
+        return {
+            'Název': data['Title'],
+            'Režisér': data['Director'],
+            'Rok vydání': data['Year'],
+            'Popis': data['Plot'],
+            'Rating': data['imdbRating'],
+            'Plakát': data['Poster']
+        }
+    else:
+        return None
+
+@bot.event
+async def on_ready():
+    print(f'Bot {bot.user.name} je online!')
+
+@bot.command()
+async def poslouchej(ctx, *, nazev_filmu):
+    kanal_id = 69,74,73,20,61,20,73,65,63,72,65,74
+    kanal = bot.get_channel(kanal_id)
+
+    if not kanal:
+        await ctx.send(f'Kanal s ID {kanal_id} nebyl nalezen.')
+        return
+
+    await kanal.send(f'Získávám informace o filmu: {nazev_filmu}...')
+    api_key = '69,74,73,20,61,20,73,65,63,72,65,74'
+
+    info = ziskej_info_o_filmu(nazev_filmu, api_key)
+    if info:
+        embed = discord.Embed(
+        title=f"{info['Název']} {info['Rok vydání']}",
+        description=info['Popis'],
+        color=discord.Color.red()
+        )
+
+        embed.add_field(name="Režisér", value=info['Režisér'], inline=False),
+        embed.add_field(name="Rating", value=f"{info['Rating']}/10", inline=False)
+
+        embed.set_image(url=info['Plakát'])
+
+        await kanal.send(embed=embed)
+    else:
+        await kanal.send(f'Film {nazev_filmu} nebyl nalezen.')
+
+bot.run('69,74,73,20,61,20,73,65,63,72,65,74')
